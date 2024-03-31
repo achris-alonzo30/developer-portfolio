9eebe9a85a806d37c78988797c68a7128f1f4519
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-03-31 19:40:56 -0300

    fix my portfolio description

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Chris Alonzo",
   profile: '/profile.jpeg',
   designation: "Full-Stack Software Developer",
-  description: "Welcome to Chris Alonzo's portfolio. I am an adept full-stack developer with a self-taught background. My passion lies in continuous learning and collaborating with fellow professionals. I thrive on new challenges and am known for my quick adaptability and learning agility.",
+  description: "Welcome to Chris Alonzo's portfolio. I am highly functional full-stack developer with a self-taught background. I have a knack for learning quickly and adapting to new technologies and I love taking on difficult tasks.",
   email: 'achris.alonzo30@gmail.com',
   phone: '+1(902) 802-1419',
   address: 'Halifax, Nova Scotia Canada',
