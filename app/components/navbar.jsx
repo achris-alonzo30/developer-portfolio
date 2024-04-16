bb57fbd168500d383c50902f61e0a9d14489126c
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-04-16 17:29:36 -0300

    2: fix the link in projects navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -27,7 +27,7 @@ function Navbar() {
             <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#education"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div></a>
           </li>
           <li>
-            <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></a>
+            <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#experience"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></a>
           </li>
         </ul>
       </div>
